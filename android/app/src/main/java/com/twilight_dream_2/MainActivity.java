2d41dc322980586eb8192ba9526604b4f9a0ae79
Crowdbotics <engineering@crowdbotics.com>
2023-01-27 14:56:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/twilight_dream_2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.twilight_dream_2;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "twilight_dream_2";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-dream-2",
+  url: "https://twilight-dream-2.botics.co",
+  api: "https://twilight-dream-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
